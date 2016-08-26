3a25c489b715365466d700465a1474dba22a475a
cmc33 <cmcguigan33@gmail.com>
2016-08-26 15:15:02 -0400

    flash messages

--- Photogram/app/controllers/posts_controller.rb
+++ Photogram/app/controllers/posts_controller.rb
@@ -1,42 +1,50 @@
-class PostsController < ApplicationController
+class PostsController < ApplicationController  
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-def new
-  @post = Post.new
-end
+  def index
+    @posts = Post.all
+  end
 
-def create
-  @post = Post.create(post_params)
-  redirect_to posts_path
-end
+  def show
+  end
 
-def index
-  @posts = Post.all
-end
+  def new
+    @post = Post.new
+  end
 
-def show  
-  @post = Post.find(params[:id])
-end  
+  def create
+    if @post = Post.create(post_params)
+      flash[:success] = "Your post has been created!"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Your new post couldn't be created! Please check the form."
+      render :new
+    end
+  end
 
-def edit
-  @post = Post.find(params[:id])
-end
+  def edit
+  end
 
-def update
-  @post = Post.find(params[:id])
-  @post.update(post_params)
-  redirect_to(post_path(@post))
-end
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
 
-def destroy  
-  @post = Post.find(params[:id])
-  @post.destroy
-  redirect_to posts_path
-end  
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
 
-private
+  private
 
-def post_params  
-  params.require(:post).permit(:image, :caption)
-end  
+  def post_params
+    params.require(:post).permit(:image, :caption)
+  end
 
-end
+  def set_post
+    @post = Post.find(params[:id])
+  end
+end 
