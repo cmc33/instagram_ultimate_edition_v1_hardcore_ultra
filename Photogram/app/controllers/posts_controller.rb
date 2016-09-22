849224de010f5ab4dc2d9d00e5d2c40ee0e69bdf
cmc33 <cmcguigan33@gmail.com>
2016-09-22 18:24:45 -0400

    owned post bug fixed

--- Photogram/app/controllers/posts_controller.rb
+++ Photogram/app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
-
+  before_action :owned_post, only: [:edit, :update, :destroy] 
   def index
     @posts = Post.all
   end
@@ -54,5 +54,11 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def owned_post  
+    unless current_user == @post.user
+    flash[:alert] = "That post doesn't belong to you!"
+    redirect_to root_path
+    end
+  end  
   
 end 
