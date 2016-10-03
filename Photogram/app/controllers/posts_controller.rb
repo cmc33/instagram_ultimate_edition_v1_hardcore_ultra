29ba1c917cfc77732cb9761f70fbe313ae0e2426
cmc33 <cmcguigan33@gmail.com>
2016-10-03 15:38:07 -0400

    improve actions

--- Photogram/app/controllers/posts_controller.rb
+++ Photogram/app/controllers/posts_controller.rb
@@ -14,17 +14,7 @@ class PostsController < ApplicationController
     @post = current_user.posts.build
   end
 
-  def create
-    @post = current_user.posts.build(post_params)
-
-    if @post.save
-      flash[:success] = "Your post has been created!"
-      redirect_to posts_path
-    else
-      flash[:alert] = "Your new post couldn't be created!  Please check the form."
-      render :new
-    end
-  end
+  
 
   def edit
   end
