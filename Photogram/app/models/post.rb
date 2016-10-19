78ce1e1be666e0fb0a627f6de7c2b076111ad023
cmc33 <cmcguigan33@gmail.com>
2016-10-19 13:08:58 -0400

    add validate length of caption

--- Photogram/app/models/post.rb
+++ Photogram/app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base  
   validates :user_id, presence: true  
   validates :image, presence: true
+  validates_length_of :caption, :minimum => 5, :maximum => 5, :allow_blank => true
   has_many :comments, dependent: :destroy
   belongs_to :user
   has_attached_file :image, styles: { :medium => "640x" }
