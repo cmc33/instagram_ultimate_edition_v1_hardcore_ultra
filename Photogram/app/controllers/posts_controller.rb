0e0b8ad5c60be47232026dd241c0add10f71fbbd
cmc33 <cmcguigan33@gmail.com>
2016-11-10 15:46:55 -0500

    bux fix

--- Photogram/app/controllers/posts_controller.rb
+++ Photogram/app/controllers/posts_controller.rb
@@ -18,7 +18,15 @@ class PostsController < ApplicationController
   end
 
   def create
-    
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      flash[:success] = "Your post has been created!"
+      redirect_to posts_path
+    else
+      flash[:alert] = "Your new post couldn't be created!  Please check the form."
+      render :new
+    end
   end
 
   def edit
