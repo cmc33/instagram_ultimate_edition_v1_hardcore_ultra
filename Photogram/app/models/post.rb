dbac1e8c6866441d4a5c7d67f96dac8df5c85c44
cmc33 <cmcguigan33@gmail.com>
2016-11-29 15:38:51 -0500

    code climate test 2

--- Photogram/app/models/post.rb
+++ Photogram/app/models/post.rb
@@ -1,1 +1,9 @@
-
+class Post < ActiveRecord::Base  
+  validates :user_id, presence: true  
+  validates :image, presence: true
+  validates_length_of :caption, :minimum => 5, :maximum => 5, :allow_blank => true
+  has_many :comments, dependent: :destroy
+  belongs_to :user
+  has_attached_file :image, styles: { :medium => "640x" }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+end  
